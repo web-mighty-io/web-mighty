e86384c184b4c2888e4c1a7d4b0adfdf17bdcfdf
Seungmin Choi <39756473+gs18113@users.noreply.github.com>
2020-04-05 12:25:05 +0900

    :construction: Created basic game structure (#7)
    * :construction: Created basic game structure
    
    Created basic game structure
    
    * :bug: typo fix
    
    typo fix
    
    * :ok_hand: name change
    
    ingame --> inGame

--- core/src/game.rs
+++ core/src/game.rs
@@ -1,5 +1,67 @@
 use crate::user::User;
+use crate::user::Card;
+
+pub enum GameState {
+    Election,
+    InGame(i32, i32), // Ingame(round starter, current player)
+    Finished(i32), // Finished(winner uid)
+}
+
+pub enum FriendCondition {
+    Card(Vec<String>), // Specific card set
+    Person(u64), // Uid of a specific person
+}
+
+pub enum Friend {
+    Known(User),
+    Unknown(FriendCondition),
+    None,
+}
+
+pub struct InGameUser {
+    pub uid: u64,
+    pub name: String,
+    in_hand: Vec<Card>,
+    gained: Vec<Card>,
+    pub score: i32,
+}
+
+impl InGameUser {
+    fn from(user: &User) -> InGameUser {
+        InGameUser {
+            uid: user.uid,
+            name: user.name.clone(),
+            in_hand: Vec::new(),
+            gained: Vec::new(),
+            score: 0,
+        }
+    }
+}
 
 pub struct Game {
-    user: User,
+    users: Vec<InGameUser>,
+    pub state: GameState,
+    pub friend : Friend,
+}
+
+impl Game {
+    pub fn new() -> Game {
+        Game{
+            users: Vec::new(),
+            state: GameState::Election,
+            friend: Friend::None,
+        }
+    }
+
+    pub fn add_user(&mut self, user: &User) {
+        self.users.push(InGameUser::from(user));
+    }
+
+    pub fn delete_user(&mut self, user: &User) {
+        self.users.retain(|x| x.uid != user.uid);
+    }
+
+    pub fn place_card(&mut self, user: &User, card: &Card) {
+        // TODO
+    }
 }

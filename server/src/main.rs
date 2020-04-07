af004da66bcf01bdfe6b0e9d6b634078d209b654
buttercrab <jaeyong0201@gmail.com>
2020-04-07 20:18:08 +0900

    :sparkles: added basic structure with clap & actix-web

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder};
+use clap::Clap;
+use std::path::PathBuf;
+
+#[derive(Clap)]
+#[clap(version = "1.0.0-dev", author = "Jaeyong Sung")]
+struct Opts {
+    #[clap(short = "i", long = "host", default_value = "127.0.0.1")]
+    host: String,
+    #[clap(long = "http-port", default_value = "80")]
+    http_port: u16,
+    #[clap(long = "https-port", default_value = "443")]
+    https_port: u16,
+    #[clap(long = "https")]
+    https: bool,
+    #[clap(short = "l", long = "log", parse(from_os_str))]
+    log: Option<PathBuf>,
+    #[clap(short = "v", long = "verbose", parse(from_occurrences))]
+    verbose: i32,
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    format!("Hello")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    let opts: Opts = Opts::parse();
+
+    HttpServer::new(|| App::new().service(index))
+        .bind(format!("{}:{}", opts.host, opts.http_port))?
+        .run()
+        .await
 }

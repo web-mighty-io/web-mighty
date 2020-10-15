65ef9d4b8e8b5d0729ec6d43eff26026ae10248b
Jaeyong Sung <jaeyong0201@gmail.com>
2020-10-15 23:34:49 +0900

    Made basic structure of game (#13)
    * :art: improving structure of game
    
    * :sparkles: made mighty game structure
    
    made structure using state

--- core/src/base.rs
+++ core/src/base.rs
@@ -1,5 +1,4 @@
-use crate::user::UserId;
-use rand::seq::SliceRandom;
+use std::str::FromStr;
 
 #[derive(PartialEq, Clone)]
 pub enum CardType {
@@ -7,114 +6,224 @@ pub enum CardType {
     Diamond,
     Heart,
     Clover,
+}
+
+pub struct ParseCardTypeError;
+
+impl FromStr for CardType {
+    type Err = ParseCardTypeError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "s" | "spade" => Ok(Self::Spade),
+            "d" | "diamond" => Ok(Self::Diamond),
+            "h" | "heart" => Ok(Self::Heart),
+            "c" | "clover" => Ok(Self::Clover),
+            _ => Err(ParseCardTypeError),
+        }
+    }
+}
+
+#[derive(PartialEq, Clone)]
+pub enum ColorType {
     Black,
     Red,
 }
 
-impl CardType {
-    pub fn contains(lhs: &CardType, rhs: &CardType) -> bool {
-        if *lhs == *rhs {
-            true
-        } else {
-            (*lhs == CardType::Red && (*rhs == CardType::Diamond || *rhs == CardType::Heart))
-                || (*lhs == CardType::Black
-                    && (*rhs == CardType::Spade || *rhs == CardType::Clover))
+pub struct ParseColorTypeError;
+
+impl FromStr for ColorType {
+    type Err = ParseColorTypeError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "b" | "black" => Ok(Self::Black),
+            "r" | "red" => Ok(Self::Red),
+            _ => Err(ParseColorTypeError),
         }
+    }
+}
+
+impl From<CardType> for ColorType {
+    fn from(c: CardType) -> Self {
+        match c {
+            CardType::Spade => Self::Black,
+            CardType::Diamond => Self::Red,
+            CardType::Heart => Self::Red,
+            CardType::Clover => Self::Black,
+        }
+    }
+}
+
+impl From<RushType> for ColorType {
+    fn from(c: RushType) -> Self {
+        match c {
+            RushType::Spade => Self::Black,
+            RushType::Diamond => Self::Red,
+            RushType::Heart => Self::Red,
+            RushType::Clover => Self::Black,
+            RushType::Red => Self::Red,
+            RushType::Black => Self::Black,
+        }
+    }
+}
+
+impl ColorType {
+    pub fn contains(lhs: ColorType, rhs: CardType) -> bool {
+        if lhs == ColorType::Black {
+            rhs == CardType::Spade || rhs == CardType::Clover
+        } else {
+            rhs == CardType::Diamond || rhs == CardType::Heart
+        }
+    }
+}
+
+#[derive(PartialEq, Clone)]
+pub enum RushType {
+    Spade,
+    Diamond,
+    Heart,
+    Clover,
+    Red,
+    Black,
+}
+
+pub struct ParseRushTypeError;
+
+impl FromStr for RushType {
+    type Err = ParseRushTypeError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "s" | "spade" => Ok(Self::Spade),
+            "d" | "diamond" => Ok(Self::Diamond),
+            "h" | "heart" => Ok(Self::Heart),
+            "c" | "clover" => Ok(Self::Clover),
+            "r" | "red" => Ok(Self::Red),
+            "b" | "black" => Ok(Self::Black),
+            _ => Err(ParseRushTypeError),
+        }
+    }
+}
+
+impl From<CardType> for RushType {
+    fn from(c: CardType) -> Self {
+        match c {
+            CardType::Spade => Self::Spade,
+            CardType::Diamond => Self::Diamond,
+            CardType::Heart => Self::Heart,
+            CardType::Clover => Self::Clover,
+        }
+    }
+}
+
+impl From<ColorType> for RushType {
+    fn from(c: ColorType) -> Self {
+        match c {
+            ColorType::Black => Self::Black,
+            ColorType::Red => Self::Red,
+        }
+    }
+}
+
+impl From<Card> for RushType {
+    fn from(c: Card) -> Self {
+        match c {
+            Card::Normal(t, _) => Self::from(t),
+            Card::Joker(t) => Self::from(t),
+        }
+    }
+}
+
+impl RushType {
+    pub fn contains(&self, c: &CardType) -> bool {
+        Self::from(c.clone()).eq(self) || Self::from(ColorType::from(c.clone())).eq(self)
     }
 }
 
 #[derive(PartialEq, Clone)]
 pub enum Card {
     Normal(CardType, u8),
-    Joker(CardType),
-    Other(String),
+    Joker(ColorType),
 }
 
-pub trait DeckTrait {
-    fn new() -> Self;
+pub struct ParseCardError;
 
-    fn get_list(&self) -> &Vec<Card>;
-
-    fn get_mut_list(&mut self) -> &mut Vec<Card>;
-
-    fn shuffle(&mut self) {
-        let v = self.get_mut_list();
-        v.shuffle(&mut rand::thread_rng());
-    }
-
-    fn next(&mut self) -> Option<Card> {
-        let v = self.get_mut_list();
-        v.pop()
+impl From<ParseCardTypeError> for ParseCardError {
+    fn from(_: ParseCardTypeError) -> Self {
+        Self
     }
 }
 
-pub trait UserTrait {
-    fn get_user_id(&self) -> UserId;
+impl From<ParseColorTypeError> for ParseCardError {
+    fn from(_: ParseColorTypeError) -> Self {
+        Self
+    }
 }
 
-pub trait StateTrait {
-    fn current_player(&self) -> Option<UserId>;
+impl FromStr for Card {
+    type Err = ParseCardError;
 
-    fn next(&mut self);
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.get(0..1).ok_or(ParseCardError)? {
+            "n" => {
+                let num = s.get(2..3).ok_or(ParseCardError)?;
+                let num = u8::from_str_radix(num, 16).map_err(|_| ParseCardError)?;
+                Ok(Self::Normal(
+                    s.get(1..2).ok_or(ParseCardError)?.parse::<CardType>()?,
+                    num,
+                ))
+            }
+            "j" => Ok(Self::Joker(
+                s.get(1..).ok_or(ParseCardError)?.parse::<ColorType>()?,
+            )),
+            _ => Err(ParseCardError),
+        }
+    }
+}
+
+impl Card {
+    /// New card deck (not shuffled)
+    pub fn new_deck() -> Vec<Card> {
+        let mut v = Vec::with_capacity(54);
+
+        for i in 1..=13 {
+            v.push(Card::Normal(CardType::Clover, i));
+        }
+        for i in 1..=13 {
+            v.push(Card::Normal(CardType::Diamond, i));
+        }
+        for i in 1..=13 {
+            v.push(Card::Normal(CardType::Heart, i));
+        }
+        for i in 1..=13 {
+            v.push(Card::Normal(CardType::Clover, i));
+        }
+
+        v.push(Card::Joker(ColorType::Black));
+        v.push(Card::Joker(ColorType::Red));
+
+        v
+    }
+}
+
+/// type of friend making
+#[derive(Clone)]
+pub enum FriendFunc {
+    None,
+    ByCard(Card),
+    ByUser(usize),
+    ByWinning(u8),
+}
+
+pub enum GameError {
+    CommandError(String),
+    InternalError(String),
 }
 
 pub trait GameTrait {
-    type Deck: DeckTrait;
-    type User: UserTrait;
-    type State: StateTrait;
+    type State;
 
-    fn is_score_card(card: &Card) -> bool {
-        match card {
-            Card::Normal(_, t) => *t >= 10,
-            Card::Joker(_) => false,
-            Card::Other(_) => false,
-        }
-    }
-
-    fn compare_cards(&self, lhs: Card, rhs: Card) -> bool {
-        if let Some(mighty) = self.get_mighty() {
-            if lhs == mighty {
-                return false;
-            }
-            if rhs == mighty {
-                return true;
-            }
-        }
-
-        if let Card::Joker(_) = lhs {
-            return self.is_joker_called();
-        }
-        if let Card::Joker(_) = rhs {
-            return !self.is_joker_called();
-        }
-
-        if let Card::Normal(left_pat, left_num) = lhs {
-            if let Card::Normal(right_pat, right_num) = rhs {
-                let a = CardType::contains(&left_pat, &self.get_current_pattern());
-                let b = CardType::contains(&right_pat, &self.get_current_pattern());
-
-                if (a && !b) || (!a && b) {
-                    return a;
-                }
-
-                return left_num < right_num;
-            }
-        }
-
-        unreachable!()
-    }
-
-    fn is_joker_called(&self) -> bool;
-
-    fn get_current_pattern(&self) -> CardType;
-
-    fn get_giruda(&self) -> Option<CardType>;
-
-    fn get_mighty(&self) -> Option<Card> {
-        match self.get_giruda() {
-            Some(CardType::Spade) => Some(Card::Normal(CardType::Heart, 1)),
-            Some(_) => Some(Card::Normal(CardType::Spade, 1)),
-            None => None,
-        }
-    }
+    // first argument in instruction is user id (always in bound)
+    fn process(&self, args: Vec<String>) -> Result<Self::State, GameError>;
 }

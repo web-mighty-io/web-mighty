0d4c57a9483b2b409810c45f19a4879c66833c3a
Jaeyong Sung <jaeyong0201@gmail.com>
2020-11-03 21:55:23 +0900

    ✨ added postgresql support (#47)

---
+++ server/src/db/register.rs
@@ -0,0 +1,58 @@
+use crate::handlers::RegisterForm;
+use actix_web::{HttpResponse, ResponseError};
+use deadpool_postgres::{Pool, PoolError};
+use derive_more::Display;
+
+#[derive(Debug, Display)]
+pub enum RegisterError {
+    PoolError(PoolError),
+    InvalidUsername,
+    UsernameExist,
+    InvalidPassword,
+}
+
+impl From<PoolError> for RegisterError {
+    fn from(e: PoolError) -> Self {
+        RegisterError::PoolError(e)
+    }
+}
+
+impl From<tokio_postgres::Error> for RegisterError {
+    fn from(e: tokio_postgres::Error) -> Self {
+        Self::from(PoolError::from(e))
+    }
+}
+
+impl ResponseError for RegisterError {
+    fn error_response(&self) -> HttpResponse {
+        match *self {
+            RegisterError::PoolError(ref err) => {
+                HttpResponse::InternalServerError().body(err.to_string())
+            }
+            RegisterError::InvalidUsername => {
+                HttpResponse::BadRequest().body("username not allowed")
+            }
+            RegisterError::UsernameExist => HttpResponse::Conflict().body("username exists"),
+            RegisterError::InvalidPassword => {
+                HttpResponse::BadRequest().body("password is not allowed")
+            }
+        }
+    }
+}
+
+// todo: change sql
+pub async fn register(form: &RegisterForm, pool: &Pool) -> Result<(), RegisterError> {
+    let client = pool.get().await?;
+    let stmt = client.prepare("SELECT id FROM user WHERE id=$1").await?;
+    let res = client.query(&stmt, &[&form.username]).await?;
+    if !res.is_empty() {
+        return Err(RegisterError::UsernameExist);
+    }
+
+    let client = pool.get().await?;
+    let stmt = client.prepare("INSERT ...").await?;
+    let _ = client
+        .query(&stmt, &[&form.username, &form.password_hash])
+        .await?;
+    Ok(())
+}
---
+++ server/src/handlers/mod.rs
@@ -0,0 +1,30 @@
+mod get;
+mod post;
+
+use actix_web::{web, HttpResponse, Responder};
+use serde::Deserialize;
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(get::index)
+        .service(post::login)
+        .service(post::logout)
+        .service(post::register)
+        .service(web::scope("/res").service(get::resource));
+}
+
+// todo: make 404 file
+pub async fn p404() -> impl Responder {
+    HttpResponse::NotFound()
+}
+
+#[derive(Deserialize)]
+pub struct LoginForm {
+    pub username: String,
+    pub password_hash: String,
+}
+
+#[derive(Deserialize)]
+pub struct RegisterForm {
+    pub username: String,
+    pub password_hash: String,
+}

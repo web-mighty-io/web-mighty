b41410bb2a75d3817740540c788e16144daca90d
Jaeyong Sung <jaeyong0201@gmail.com>
2021-01-31 18:09:16 +0900

    :rotating_light: fixed user.js

--- static/res/js/user.js
+++ static/res/js/user.js
@@ -1,8 +1,26 @@
 export let user = {
-    async login(username, password) {
+    async login(username, password, on_error) {
+        let res = await fetch("/login", {
+            method: "post",
+            headers: {
+                "Accept": "application/json, text/plain, */*",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                user_id: username,
+                password: password,
+            })
+        });
+        if (res.ok) {
+            let params = new URLSearchParams(window.location.search);
+            let redirect_to = "/";
+            if (params.has("back")) {
+                redirect_to = params.get("back");
+            }
+            window.location.replace(redirect_to);
+        } else {
+            on_error(res.text());
+        }
 
     },
-    async register(username, email) {
-
-    }
 };

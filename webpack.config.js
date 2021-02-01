3832211e7e7b23e11f00e334ebbbdbe77e7a1a9b
Jaeyong Sung <jaeyong0201@gmail.com>
2021-02-02 02:10:16 +0900

    :wrench: fixed dockerfile

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = (env) => {
 
     return {
         mode: "production",
-        entry: glob.sync("./public/js/*.js").reduce((acc, item) => {
+        entry: glob.sync(path.resolve(__dirname, "public", "js", "*.js")).reduce((acc, item) => {
             const path = item.split("/");
             const name = path[path.length - 1].split(".").slice(0, -1).join(".");
             acc[name] = item;

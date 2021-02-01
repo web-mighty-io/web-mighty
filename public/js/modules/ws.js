4038be081a3e894ba9faa15e69790dad967b270b
Jaeyong Sung <jaeyong0201@gmail.com>
2021-02-02 01:29:03 +0900

    :fire: removed commented codes

--- public/js/modules/ws.js
+++ public/js/modules/ws.js
@@ -1,19 +1,3 @@
-// import * as wasm from "../pkg";
-//
-// let main;
-//
-// await (async function () {
-//     await wasm.run();
-//
-//     main = new wasm.Main();
-// })();
-//
-// let ws = {
-//     main
-// };
-//
-// export default ws;
-
 let ws = import ("../pkg/index").then(wasm => {
     wasm.run()
 

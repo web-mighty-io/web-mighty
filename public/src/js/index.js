c1b51a7fbfb761c2fed75b3dc1ba96886a9d881f
Jaeyong Sung <jaeyong0201@gmail.com>
2021-02-10 23:34:42 +0900

    :lipstick: added front page

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -2,5 +2,14 @@ import "./modules/wasm.js";
 import "../scss/index.scss";
 
 window.onload = function () {
-    console.log("Hello");
+    let loginButton = document.getElementById("login-button");
+    let registerButton = document.getElementById("register-button");
+
+    loginButton.addEventListener("click", function () {
+        window.location.replace("/login");
+    });
+
+    registerButton.addEventListener("click", function () {
+        window.location.replace("/register");
+    });
 };

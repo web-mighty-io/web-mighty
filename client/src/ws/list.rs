30ab8d50d2c894d32d31b8e31b66c191528e213c
Godthink_un <44489551+myeongjinshin@users.noreply.github.com>
2021-02-16 23:44:15 +0900

    made simple get list

--- client/src/ws/list.rs
+++ client/src/ws/list.rs
@@ -15,6 +15,7 @@ impl SessionTrait for ListSession {
         let msg: ListToClient = serde_json::from_str(&*msg).unwrap();
         match msg {
             ListToClient::Room(room_info) => ("", JsValue::from_serde(&room_info).unwrap()),
+            ListToClient::RoomList(room_list) => ("", JsValue::from_serde(&room_list).unwrap()),
         }
     }
 }
@@ -43,5 +44,11 @@ impl List {
 
     pub fn unsubscribe(&self, room_id: RoomId) {
         self.session.send(ListToServer::Unsubscribe(room_id));
+    }
+
+    pub fn get_room_list(&self, user_min: u32, user_max: u32) {
+        self.session.send(ListToServer::GetRoomList {
+            user_num: (user_min, user_max),
+        });
     }
 }
